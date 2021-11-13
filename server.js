e1706727cba8f8e578fff6ed67e4ca03e6bd1aae
DoodleBob <riley.foust18@gmail.com>
2021-11-13 15:42:47 -0500

    added server and index files with connection logic

---
+++ server.js
@@ -0,0 +1,25 @@
+const db = require("./db/connection");
+const apiRoutes = require("./routes/apiRoutes");
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// express middleware
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.use("/api", apiRoutes);
+
+// Default response for any other request (Not found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+// Start server after DB connection
+db.connect(err => {
+  if (err) throw err;
+  console.log('Database connected.');
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
