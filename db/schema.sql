22a7e01bce7c877d7665350f5b6fc994dd67a95f
DoodleBob <riley.foust18@gmail.com>
2021-11-13 15:28:04 -0500

    reworked seeds file and filled with data

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS dpartments;
+DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS employees;
 
 CREATE TABLE departments (
@@ -12,7 +12,7 @@ CREATE TABLE roles (
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INTEGER,
-  CONSTRAINT fk_party FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
@@ -20,5 +20,7 @@ CREATE TABLE employees (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INTEGER,
-  CONSTRAINT fk_party FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
+  manager_id INTEGER,
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,28 +1,28 @@
 INSERT INTO departments (name)
   VALUES
-    ("Sales")
-    ("Engineering")
-    ("Finance")
-    ("Legal")
+    ("Sales"),
+    ("Engineering"),
+    ("Finance"),
+    ("Legal");
 
-INSERT INTO roles (title, salaray, department_id)
+INSERT INTO roles (title, salary, department_id)
   VALUES 
-      ("Sales Lead, 100000", 1)    
-      ("Salesperson, 80000", 1)
-      ("Lead Engineer, 150000", 2)
-      ("Software Engineer, 120000", 2)
-      ("Account Manager, 200000", 3)
-      ("Accountant, 125000", 3)
-      ("Legal Team Lead, 250000", 4) 
-      ("Lawyer, 190000", 4)
+      ("Sales Lead", 100000, 1),    
+      ("Salesperson", 80000, 1),
+      ("Lead Engineer", 150000, 2),
+      ("Software Engineer", 120000, 2),
+      ("Account Manager", 200000, 3),
+      ("Accountant", 125000, 3),
+      ("Legal Team Lead", 250000, 4),
+      ("Lawyer", 190000, 4);
 
-INSERT INTO employees (first_name, last_name, role_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
   VALUES
-    ("Benny Foster", 1)
-    ("Pamela Myers", 2)
-    ("Vicky Hanson", 3)
-    ("Simon Price", 4)
-    ("Roxanne Brady", 5)
-    ("Emanuel Brewer", 6)
-    ("Josefina Jefferson", 7)
-    ("Rogelio Rodriguez", 8)
+    ("Benny", "Foster", 1),
+    ("Pamela", "Myers", 2, 1),
+    ("Vicky", "Hanson", 3),
+    ("Simon", "Price", 4, 3),
+    ("Roxanne", "Brady", 5),
+    ("Emanuel", "Brewer", 6, 5),
+    ("Josefina", "Jefferson", 7),
+    ("Rogelio", "Rodriguez", 8, 7);
